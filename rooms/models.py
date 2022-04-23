328459fe1436997fd62e0995c3443a6060749a4f
jeongmingi <mo4811@naver.com>
2022-04-23 17:04:35 +0900

    Update models.py

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,39 @@
 from django.db import models
 from django_countries.fields import CountryField
+
+import core.models
 from core import models as core_models
 from users import models as user_models
+
+
+class AbstractItem(core.models.TimeStampedModel):
+    """ Abstarct Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    pass
+
+
+class Amenity(AbstractItem):
+    pass
+
+
+class Facility(AbstractItem):
+    """ Facility Model Definition """
+
+
+class HouseRule(AbstractItem):
+    """ House Model Definition """
+
+    pass
 
 
 class Room(core_models.TimeStampedModel):
@@ -18,3 +50,7 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    facilities = models.ManyToManyField(Facility)
+    house_rules  = models.ManyToManyField(HouseRule)
